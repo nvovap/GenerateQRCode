0742e3f87b998ddaccd2a87c782fb2ff67747f3d
Vladimir Nevinniy <nvovap@gmail.com>
2017-06-18 17:07:24 +0300

    18062017

---
+++ GenerateQRCode/DetectionQRCodeViewController.swift
@@ -0,0 +1,110 @@
+//
+//  DetectionQRCodeViewController.swift
+//  GenerateQRCode
+//
+//  Created by Владимир Невинный on 18.06.17.
+//  Copyright © 2017 Владимир Невинный. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class DetectionQRCodeViewController: UIViewController {
+    
+    var QRCodeForReturn: String = ""
+
+    var captureSession: AVCaptureSession!
+    var previewLayer: AVCaptureVideoPreviewLayer!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = UIColor.black
+        captureSession = AVCaptureSession()
+        
+        let videoCaptureDevice = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
+        let videoInput: AVCaptureDeviceInput
+        
+        do {
+            videoInput = try AVCaptureDeviceInput(device: videoCaptureDevice)
+        } catch {
+            return
+        }
+        
+        if (captureSession.canAddInput(videoInput)) {
+            captureSession.addInput(videoInput)
+        } else {
+            failed();
+            return;
+        }
+        
+        let metadataOutput = AVCaptureMetadataOutput()
+        
+        if (captureSession.canAddOutput(metadataOutput)) {
+            captureSession.addOutput(metadataOutput)
+            
+            metadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+            metadataOutput.metadataObjectTypes = [AVMetadataObjectTypeQRCode]
+        } else {
+            failed()
+            return
+        }
+        
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession);
+        previewLayer.frame = view.layer.bounds;
+        previewLayer.videoGravity = AVLayerVideoGravityResizeAspectFill;
+        view.layer.addSublayer(previewLayer);
+        
+        captureSession.startRunning();
+        
+    }
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        if (captureSession?.isRunning == false) {
+            captureSession.startRunning();
+        }
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        if (captureSession?.isRunning == true) {
+            captureSession.stopRunning();
+        }
+    }
+    
+    
+    func failed() {
+        let ac = UIAlertController(title: "Scanning not supported", message: "Your device does not support scanning a code from an item. Please use a device with a camera.", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+        captureSession = nil
+    }
+
+    
+}
+
+extension DetectionQRCodeViewController: AVCaptureMetadataOutputObjectsDelegate {
+    
+    
+    func captureOutput(_ captureOutput: AVCaptureOutput!, didOutputMetadataObjects metadataObjects: [Any]!, from connection: AVCaptureConnection!) {
+        captureSession.stopRunning()
+        
+        if let metadataObject = metadataObjects.first {
+            let readableObject = metadataObject as! AVMetadataMachineReadableCodeObject;
+            
+            AudioServicesPlaySystemSound(SystemSoundID(kSystemSoundID_Vibrate))
+            found(code: readableObject.stringValue);
+        }
+        
+        performSegue(withIdentifier: "exit", sender: self)
+    }
+    
+    func found(code: String) {
+        print(code)
+    }
+
+}
--- GenerateQRCode/ViewController.swift
+++ GenerateQRCode/ViewController.swift
@@ -9,17 +9,60 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var imageView: UIImageView!
 
+    @IBOutlet weak var detectionQRCode: UITextField!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+        let tap = UITapGestureRecognizer(target: self, action:  #selector(self.tapHandle) )
+        
+        self.view.addGestureRecognizer(tap)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func unwindToMain(segue:UIStoryboardSegue) {
+        guard let vc = segue.destination as? DetectionQRCodeViewController else {
+            return
+        }
+        
+        detectionQRCode.text = vc.QRCodeForReturn
     }
+    
+    func tapHandle() {
+        textField.resignFirstResponder()
+    }
+    
+    func generateQRCode(from string: String) -> UIImage? {
+        
+        let data = string.data(using: String.Encoding.isoLatin1)
+        
+        if let filter = CIFilter(name: "CIQRCodeGenerator") {
+            filter.setValue(data, forKey: "inputMessage")
+            let transform = CGAffineTransform(scaleX: 3, y: 3)
+            
+            if let output = filter.outputImage?.applying(transform) {
+                return UIImage(ciImage: output)
+            }
+        }
+        
+        return nil
+    }
+}
 
-
+extension ViewController: UITextFieldDelegate {
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if let text = textField.text {
+            if let image = generateQRCode(from: text) {
+                imageView.image = image
+            }
+        }
+    }
+    
 }
 
